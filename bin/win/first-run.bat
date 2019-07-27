1b904ec7e7ce0c0b8962f20c7dd3a73a37f4136b
skellers21 <iain21lawrie@gmail.com>
2019-07-27 14:07:36 +0100

    testnet removed from default anon.conf

--- bin/win/first-run.bat
+++ bin/win/first-run.bat
@@ -13,7 +13,6 @@ IF NOT EXIST %AppData%\Anon\anon.conf (
     echo rpcuser=anonuser
     echo rpcpassword=anonpass
     echo rpcallowip=127.0.0.1
-    echo testnet=1
     echo txindex=1
     echo server=1
 ) > %AppData%\Anon\anon.conf
